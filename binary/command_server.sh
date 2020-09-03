2afd48e0b4f9550af675b6836edbddf2ab666330
projectscorpio <b1xpc0@gmail.com>
2020-09-03 21:54:27 +0530

    binary

--- binary/command_server.sh
+++ binary/command_server.sh
@@ -1,2 +1,2 @@
-nohup java -Dspring.profiles.active=bp -Dspring.jpa.properties.hibernate.hbm2ddl.auto=update -Dlog.file.location=$HOME/logs/marketplace-web -jar GharPe-1.0.0.jar </dev/null >nohup.out 2>nohup.err &
+nohup java -Dspring.profiles.active=bp -Dspring.jpa.properties.hibernate.hbm2ddl.auto=create -Dlog.file.location=$HOME/logs/marketplace-web -jar appliedmind-0.0.1-SNAPSHOT.jar </dev/null >nohup.out 2>nohup.err &
 
